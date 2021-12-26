114533d09716e8a2817b868bc2210662553185a2
mario237 <mariomamdouh237@gmail.com>
2021-12-26 12:39:56 +0200

    Make Some Modification on dashboard theme

--- resources/views/layouts/main-header.blade.php
+++ resources/views/layouts/main-header.blade.php
@@ -3,18 +3,16 @@
 				<div class="container-fluid">
 					<div class="main-header-left ">
 						<div class="responsive-logo">
-							<a href="{{ url('/' . $page='index') }}"><img src="{{URL::asset('assets/img/brand/logo.png')}}" class="logo-1" alt="logo"></a>
-							<a href="{{ url('/' . $page='index') }}"><img src="{{URL::asset('assets/img/brand/logo-white.png')}}" class="dark-logo-1" alt="logo"></a>
-							<a href="{{ url('/' . $page='index') }}"><img src="{{URL::asset('assets/img/brand/favicon.png')}}" class="logo-2" alt="logo"></a>
-							<a href="{{ url('/' . $page='index') }}"><img src="{{URL::asset('assets/img/brand/favicon.png')}}" class="dark-logo-2" alt="logo"></a>
+							<a href="{{ url('/' . $page='index') }}"><img src="{{ asset('assets/img/invoice.png') }}" class="logo-1" alt="logo"></a>
+							<a href="{{ url('/' . $page='index') }}"><img src="{{ asset('assets/img/invoice.png') }}" class="dark-logo-1" alt="logo"></a>
+							<a href="{{ url('/' . $page='index') }}"><img src="{{ asset('assets/img/invoice.png') }}" class="logo-2" alt="logo"></a>
+							<a href="{{ url('/' . $page='index') }}"><img src="{{ asset('assets/img/invoice.png') }}" class="dark-logo-2" alt="logo"></a>
 						</div>
 						<div class="app-sidebar__toggle" data-toggle="sidebar">
 							<a class="open-toggle" href="#"><i class="header-icon fe fe-align-left" ></i></a>
 							<a class="close-toggle" href="#"><i class="header-icons fe fe-x"></i></a>
 						</div>
-						<div class="main-header-center mr-3 d-sm-none d-md-none d-lg-block">
-							<input class="form-control" placeholder="Search for anything..." type="search"> <button class="btn"><i class="fas fa-search d-none d-md-block"></i></button>
-						</div>
+
 					</div>
 					<div class="main-header-right">
 						<ul class="nav">
@@ -254,7 +252,7 @@
 										<div class="d-flex wd-100p">
 											<div class="main-img-user"><img alt="" src="{{URL::asset('assets/img/faces/6.jpg')}}" class=""></div>
 											<div class="mr-3 my-auto">
-												<h6>Petey Cruiser</h6><span>Premium Member</span>
+												<h6>{{ Auth::user()->name }}</h6><span>Admin</span>
 											</div>
 										</div>
 									</div>
@@ -263,14 +261,17 @@
 									<a class="dropdown-item" href=""><i class="bx bxs-inbox"></i>Inbox</a>
 									<a class="dropdown-item" href=""><i class="bx bx-envelope"></i>Messages</a>
 									<a class="dropdown-item" href=""><i class="bx bx-slider-alt"></i> Account Settings</a>
-									<a class="dropdown-item" href="{{ url('/' . $page='page-signin') }}"><i class="bx bx-log-out"></i> Sign Out</a>
-								</div>
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();document.getElementById('logout-form').submit();"><i
+                                            class="bx bx-log-out"></i>Sign Out</a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+
+
+                                </div>
 							</div>
-							<div class="dropdown main-header-message right-toggle">
-								<a class="nav-link pr-0" data-toggle="sidebar-left" data-target=".sidebar-left">
-									<svg xmlns="http://www.w3.org/2000/svg" class="header-icon-svgs" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
-								</a>
-							</div>
+
 						</div>
 					</div>
 				</div>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -16,17 +16,16 @@
 			<img src="{{URL::asset('assets/img/loader.svg')}}" class="loader-img" alt="Loader">
 		</div>
 		<!-- /Loader -->
-		@include('layouts.main-sidebar')		
+		@include('layouts.main-sidebar')
 		<!-- main-content -->
 		<div class="main-content app-content">
-			@include('layouts.main-header')			
+			@include('layouts.main-header')
 			<!-- container -->
 			<div class="container-fluid">
 				@yield('page-header')
 				@yield('content')
-				@include('layouts.sidebar')
 				@include('layouts.models')
             	@include('layouts.footer')
-				@include('layouts.footer-scripts')	
+				@include('layouts.footer-scripts')
 	</body>
 </html>
